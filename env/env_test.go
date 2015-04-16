01d71dc9fb03852a1d1fe10d5346ed25eb8a10f3
Muhammed Thanish <mnmtanish@gmail.com>
2015-04-16 10:31:56 +0530

    Add tests for ints/floats/string-slice

--- env/env_test.go
+++ env/env_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"reflect"
 	"testing"
 )
 
@@ -69,6 +70,139 @@ func TestB(t *testing.T) {
 	if err != nil {
 		t.Error("should not return error")
 	} else if val != res {
+		t.Error("should return correct value")
+	}
+}
+
+func TestI(t *testing.T) {
+	key := randstr()
+
+	val, err := I(key)
+	if err == nil {
+		t.Error("should return error")
+	} else if val != 0 {
+		t.Error("should return empty value")
+	}
+
+	req := "123"
+	var res int = 123
+
+	os.Setenv(key, req)
+	val, err = I(key)
+	if err != nil {
+		t.Error("should not return error")
+	} else if val != res {
+		t.Error("should return correct value")
+	}
+}
+
+func TestI32(t *testing.T) {
+	key := randstr()
+
+	val, err := I32(key)
+	if err == nil {
+		t.Error("should return error")
+	} else if val != 0 {
+		t.Error("should return empty value")
+	}
+
+	req := "123"
+	var res int32 = 123
+
+	os.Setenv(key, req)
+	val, err = I32(key)
+	if err != nil {
+		t.Error("should not return error")
+	} else if val != res {
+		t.Error("should return correct value")
+	}
+}
+
+func TestI64(t *testing.T) {
+	key := randstr()
+
+	val, err := I64(key)
+	if err == nil {
+		t.Error("should return error")
+	} else if val != 0 {
+		t.Error("should return empty value")
+	}
+
+	req := "123"
+	var res int64 = 123
+
+	os.Setenv(key, req)
+	val, err = I64(key)
+	if err != nil {
+		t.Error("should not return error")
+	} else if val != res {
+		t.Error("should return correct value")
+	}
+}
+
+func TestF32(t *testing.T) {
+	key := randstr()
+
+	val, err := F32(key)
+	if err == nil {
+		t.Error("should return error")
+	} else if val != 0 {
+		t.Error("should return empty value")
+	}
+
+	req := "12.34"
+	var res float32 = 12.34
+
+	os.Setenv(key, req)
+	val, err = F32(key)
+	if err != nil {
+		t.Error("should not return error")
+	} else if val != res {
+		t.Error("should return correct value")
+	}
+}
+
+func TestF64(t *testing.T) {
+	key := randstr()
+
+	val, err := F64(key)
+	if err == nil {
+		t.Error("should return error")
+	} else if val != 0 {
+		t.Error("should return empty value")
+	}
+
+	req := "12.34"
+	var res float64 = 12.34
+
+	os.Setenv(key, req)
+	val, err = F64(key)
+	if err != nil {
+		t.Error("should not return error")
+	} else if val != res {
+		t.Error("should return correct value")
+	}
+}
+
+func TestSS(t *testing.T) {
+	key := randstr()
+	sep := ","
+
+	val, err := SS(key, sep)
+	if err == nil {
+		t.Error("should return error")
+	} else if val != nil {
+		t.Error("should return empty value")
+	}
+
+	req := "a,b,c"
+	res := []string{"a", "b", "c"}
+
+	os.Setenv(key, req)
+	val, err = SS(key, sep)
+	if err != nil {
+		t.Error("should not return error")
+	} else if !reflect.DeepEqual(val, res) {
 		t.Error("should return correct value")
 	}
 }
